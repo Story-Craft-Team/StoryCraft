e7047fde0d059186c0ecd0d697914e70d8bb4b54
Monobladegg <fushersf2@gmail.com>
2025-05-06 07:58:01 +0300

    Init

---
+++ frontend/src/pages/Create/index.tsx
@@ -0,0 +1,11 @@
+import Chapters from "@/widgets/Create/Chapters";
+import Header from "@/widgets/Create/Header";
+
+export default function CreatePage() {
+  return (
+    <div>
+      <Header />
+      <Chapters />
+    </div>
+  );
+}
---
+++ frontend/src/pages/Home/index.tsx
@@ -0,0 +1,5 @@
+import Stories from "@/widgets/Home/Stories";
+
+export default function HomePage() {
+  return <div><Stories /></div>;
+}
