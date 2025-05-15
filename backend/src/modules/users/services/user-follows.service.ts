ff358f764be39d8d3975d171a48c01c136fcba89
Redefinedgg <fushersf2@gmail.com>
2025-05-15 14:43:32 +0300

    Added untested follow functions

--- backend/src/modules/users/services/user-follows.service.ts
+++ backend/src/modules/users/services/user-follows.service.ts
@@ -1,17 +1,43 @@
+import { BadRequestException } from "@nestjs/common";
 import { PrismaService } from "src/modules/prisma/prisma.service";
 
 export class UserFollowsService {
     constructor (private readonly prisma: PrismaService) {}
 
     // follow to the user
-    // async followToUser(userId: number, followerId: number) {
-    //     try {
-    //         await this.prisma.user.update({
-    //             where: { id: userId },
-    //             data: { followers: { connect: { id: followerId } } },
-    //         });
-    //     } catch (error) {
-    //         throw new Error('Error following user: ' + error.message);
-    //     }
-    // }
+    async followToUser(userId: number, followerId: number) {
+        try {
+            await this.prisma.user.update({
+                where: { id: userId },
+                data: { followedUsers: { connect: { id: followerId } } },
+            });
+            await this.prisma.user.update({
+                where: { id: followerId },
+                data: { followingUsers: { connect: { id: userId } } },
+            });
+        } catch (error) {
+            throw new BadRequestException(
+                `Error following user with ID ${userId}: ${error.message}`,
+            );
+        }
+    }
+
+    async unfollowUser(userId: number, followerId: number) {
+        try {
+            await this.prisma.user.update({
+                where: { id: userId },
+                data: { followedUsers: { disconnect: { id: followerId } } },
+            });
+            await this.prisma.user.update({
+                where: { id: followerId },
+                data: { followingUsers: { disconnect: { id: userId } } },
+            });
+        } catch (error) {
+            throw new BadRequestException(
+                `Error unfollowing user with ID ${userId}: ${error.message}`,
+            );
+        }
+    }
+
+    // TODO: Test all
 }
