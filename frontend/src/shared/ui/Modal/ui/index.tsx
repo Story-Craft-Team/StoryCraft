5c07cddd5fee73205a5f934ff281800ba99dfb0c
CrazyAnry <ivasenkoandrej76@gmail.com>
2025-05-15 13:57:45 +0700

    Layout fix for modal window

--- frontend/src/shared/ui/Modal/ui/index.tsx
+++ frontend/src/shared/ui/Modal/ui/index.tsx
@@ -8,9 +8,12 @@ interface Props{
 export default function Modal({children}:Props){
     const [windowIsVisible, setWindowVisible] = useState<boolean>(true)
 
+    const changeVisible = () => windowIsVisible? setWindowVisible(false) : setWindowVisible(true)
+
     return (
-        <div className={windowIsVisible? s.modalOverlay : s.containerUnVisible} onClick={() => windowIsVisible? setWindowVisible(false) : setWindowVisible(true)}>
-            <div className={windowIsVisible? s.containerVisible : s.containerUnVisible}>
+        <div className={windowIsVisible? s.modalOverlay : s.containerUnVisible} onClick={changeVisible}>
+            <div className={windowIsVisible? s.containerVisible : s.containerUnVisible} onClick={(e:React.MouseEvent) => e.stopPropagation()}>
+                <button className={s.btn} onClick={changeVisible}>Close</button>
                 {children}
             </div>
         </div>
